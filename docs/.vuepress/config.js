9f7f9b5e7379bfc9742f8518e967f6d9974c1897
lirc572 <lirc572@gmail.com>
2020-05-19 22:17:11 +0000

    updated vuepress config

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,35 @@
+const getConfig = require("vuepress-bar");
+const barConfig = getConfig(
+    `${__dirname}/..`,
+    {skipEmptySidebar: true, 
+    addReadMeToFirstGroup:false}
+)
+
+module.exports = {
+    title: 'Homital',
+    description: 'The ultimate smart home ecosystem',
+    themeConfig: {
+        //logo: 'logo.png', //navbar logo
+        smoothScroll: true,
+        nav: [
+            { text: 'Home', link: '/' },
+            {
+                text: 'Guide',
+                ariaLabel: 'Guide Menu',
+                items: [
+                    { text: 'User Guide', link: '/guide/user-guide/' },
+                    { text: 'Developer Guide', link: '/guide/developer-guide/' },
+                    { text: 'Business Guide', link: '/guide/business-guide/' }
+                ]
+            }
+        ],
+        sidebar: barConfig.sidebar,
+        lastUpdated: 'Last Updated', // string | boolean
+        repo: 'Homital/Homital.github.io',
+        docsRepo: 'Homital/Homital.github.io',
+        docsDir: 'docs',
+        docsBranch: 'docs',
+        editLinks: true,
+        editLinkText: 'Help us improve this page!'
+    }
+}
